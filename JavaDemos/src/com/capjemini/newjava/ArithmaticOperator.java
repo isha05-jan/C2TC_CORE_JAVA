b39d7918933bbc021a5be163cf3d7aef3d233026
HP <HP@DESKTOP-IUFIH7K>
2023-01-14 12:37:35 +0530

    First Commiit

---
+++ JavaDemos/src/com/capjemini/newjava/ArithmaticOperator.java
@@ -0,0 +1,14 @@
+package com.capjemini.newjava;
+
+public class ArithmaticOperator {
+
+	public static void main(String[] args) {
+		int x = 10, y=20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+
+
+	}
+
+}
