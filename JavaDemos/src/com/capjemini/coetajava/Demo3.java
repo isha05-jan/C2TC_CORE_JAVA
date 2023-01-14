3732077783bb5bb01950fa4408c76f8bf9a25f55
HP <HP@DESKTOP-IUFIH7K>
2023-01-14 13:05:29 +0530

    first commit

---
+++ JavaDemos/src/com/capjemini/coetajava/Demo3.java
@@ -0,0 +1,19 @@
+package com.capjemini.coetajava;
+import java.time.LocalDate;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		LocalDate date1=LocalDate.of(2022, 2, 28);
+		System.out.println(date1);
+		
+		LocalDate date2=LocalDate.of(2020, 2, 28);
+		System.out.println(date1);
+		
+		System.out.println(date1.isLeapYear());
+		System.out.println(date2.isLeapYear());
+		
+
+	}
+
+}
